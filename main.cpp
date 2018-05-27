2f31a1b9e197e96b9b8b4f9795614b03465c680f
kkobylin <37050099+kkobylin@users.noreply.github.com>
2018-05-27 14:17:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,4 @@ using namespace std;
 int main()
 {
     Simulation("dane.txt");
-    /*
-    ofstream writ;
-    ofstream write("results.txt", ios::app);
-    writ.open("results.txt", ios::app);
-   // writ.seekp( 0, std::ios_base::end );
-    writ<<"Patient's data: "<<endl;
-    writ<<"Patient's data: "<<endl;
-    writ<<"Patient's data: "<<endl;
-    write<<"cos"<<endl;
-    write<<"dwa"<<endl;
-    //writ<<name<<endl;
-    writ.close();
-    write.close();
-    */
 }
